d34c8849f4b59e038c868b9f04db0e3070c487a1
Peter Joles <jolzee@gmail.com>
2020-10-05 12:03:12 -0500

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv-defaults").config();
 
 const oas = require("fastify-oas");
 const fastify = require("fastify")({
-  http2: false,
+  http2: true,
   trustProxy: true,
 });
 const oauthPlugin = require("fastify-oauth2");
